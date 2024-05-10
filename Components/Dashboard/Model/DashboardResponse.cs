c060b61548e2c33bf698d67c5486e0a6148ad831
Claudio Volnei <claudiovolneisilva@gmail.com>
2024-05-09 22:40:55 -0300

    tela inicial dashboard

---
+++ Components/Dashboard/Model/DashboardResponse.cs
@@ -0,0 +1,8 @@
+namespace FinancialManager.Web.Components.Dashboard.Model
+{
+    public record struct DashboardResponse(
+        decimal Amount,
+        decimal Entrys,
+        decimal Outputs,
+        IList<Tuple<string, double>> Categories);
+}
