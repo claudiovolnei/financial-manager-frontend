9c2d9ae653802465d6652fdd606d80f3f8be7922
Claudio Volnei <claudiovolneisilva@gmail.com>
2024-05-16 20:42:13 -0300

    Corrige lista

--- Components/Dashboard/Model/DashboardResponse.cs
+++ Components/Dashboard/Model/DashboardResponse.cs
@@ -4,5 +4,5 @@ namespace FinancialManager.Web.Components.Dashboard.Model
         decimal Amount,
         decimal Entrys,
         decimal Outputs,
-        IList<Tuple<string, decimal>> Categories);
+        List<Tuple<string, decimal>> Categories);
 }
